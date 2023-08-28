1ae5d4a1fca3bd317c2b35a9083d254323a775f8
Rod Vagg <rod@vagg.org>
2023-08-28 14:10:19 +1000

    chore: update deps

--- coding_test.go
+++ coding_test.go
@@ -21,7 +21,7 @@ func TestDecode(t *testing.T) {
 	id, err := cid.Prefix{
 		Version:  1,
 		Codec:    cid.Raw,
-		MhType:   mh.ID,
+		MhType:   mh.IDENTITY,
 		MhLength: 0,
 	}.Sum(nil)
 
@@ -59,7 +59,7 @@ func TestRegistryDecode(t *testing.T) {
 	id, err := cid.Prefix{
 		Version:  1,
 		Codec:    cid.Raw,
-		MhType:   mh.ID,
+		MhType:   mh.IDENTITY,
 		MhLength: 0,
 	}.Sum(nil)
 
